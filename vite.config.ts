036245934dc1b8e72366d9b4978345c0bff90e4e
PabloViniegra <pablovpmadrid@gmail.com>
2025-05-18 15:32:10 +0200

    feat: add url and rollup-plugin-node-polyfills dependencies, update Vite configuration for improved module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsConfigPaths from 'vite-tsconfig-paths'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
+import polyfillNode from "rollup-plugin-node-polyfills";
 import path from "path";
 import { fileURLToPath } from "url";
-// https://vitejs.dev/config/
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-    plugins: [react(), tsConfigPaths()],
+    plugins: [
+        react(),
+        tsConfigPaths(),
+    ],
     resolve: {
-        alias: {
-            "#minpath": path.resolve(__dirname, "node_modules/vfile/lib/minpath.js"),
-            "#minproc": path.resolve(__dirname, "node_modules/vfile/lib/minproc.js"),
-            "#minurl": path.resolve(__dirname, "node_modules/vfile/lib/minurl.js"),
-        },
+        alias: [
+            { find: "node:url", replacement: "url" },
+            { find: "#minpath", replacement: path.resolve(__dirname, "node_modules/vfile/lib/minpath.js") },
+            { find: "#minurl", replacement: path.resolve(__dirname, "node_modules/vfile/lib/minurl.js") },
+            { find: "#minproc", replacement: path.resolve(__dirname, "node_modules/vfile/lib/minproc.js") },
+        ],
+    },
+    optimizeDeps: {
+        include: ["url"],
     },
     build: {
         rollupOptions: {
-            external: ["#minpath", "#minproc", "#minurl"],
+            plugins: [
+                polyfillNode(),
+            ],
         },
     },
-})
+});
