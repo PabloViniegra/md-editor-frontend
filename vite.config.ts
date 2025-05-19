db882263c01ffcf08c26e31661542ab3db1348a1
PabloViniegra <pablovpmadrid@gmail.com>
2025-05-19 15:59:20 +0200

    feat: add 'url' dependency and update Vite configuration for improved module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
-// https://vitejs.dev/config/
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
-    plugins: [react(), tsConfigPaths()],
+    plugins: [
+        react(),
+        tsConfigPaths(),
+    ],
+    resolve: {
+        alias: [
+            { find: '#minpath', replacement: path.resolve(__dirname, 'node_modules/vfile/lib/minpath.js') },
+            { find: '#minurl', replacement: path.resolve(__dirname, 'node_modules/vfile/lib/minurl.js') },
+            { find: '#minproc', replacement: path.resolve(__dirname, 'node_modules/vfile/lib/minproc.js') },
+        ]
+    },
+    build: {
+        rollupOptions: {
+            external: ['#minpath', '#minurl', '#minproc'],
+        }
+    }
 })
